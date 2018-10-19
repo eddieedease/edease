be50784017b9182b95e0d3d249fc3fa64ff27400
eddieedease <eddie@edease.nl>
2018-10-19 13:40:10 +0200

    workingtheme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { WorkComponent } from './work/work.component';
+
+const appRoutes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  },
+  { path: '**', component: LandingComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -12,7 +32,19 @@ import { WorkComponent } from './work/work.component';
     WorkComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
+    TabsModule.forRoot(),
+    ModalModule.forRoot(),
+    CollapseModule.forRoot(),
+    ButtonsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
