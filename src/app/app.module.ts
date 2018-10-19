fd78f00103e1f65a3e405dfde3ab4fb0659716e0
eddieedease <eddie@edease.nl>
2018-10-19 11:26:18 +0200

    startercode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule
